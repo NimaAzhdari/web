530bfbfd837f7ca196a76e3c3066abdebbfbf828
NimaAzhdari <ajdarinima@gmail.com>
2024-07-17 13:57:15 +0330

    Delete MvcDapper - Copy directory

--- MvcDapper - Copy/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace MvcDapper.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- MvcDapper - Copy/Models/LoginViewModel.cs
+++
@@ -1,6 +0,0 @@
-namespace MvcDapper.Models;
-public class LoginViewModel
-{
-    public required string Username { get; set; }
-    public required string Password { get; set; }
-}
