f362474c1437de9232d5bae423af5f7dba8a9a38
NimaAzhdari <ajdarinima@gmail.com>
2024-09-10 00:57:18 -0700

    Add files via upload

---
+++ RazorPage/Pages/Shared/Components/Basket/BasketViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+namespace MvcRazor.Pages.Shared.Components.Basket;
+
+public class BasketViewComponent : ViewComponent
+{
+
+    public IViewComponentResult  Invoke()
+    {
+
+         Models.Basket basket=new();
+        if(Request.Cookies[nameof(Basket)] is not null)
+        {
+            basket=JsonSerializer.Deserialize<Models.Basket>(Request.Cookies[nameof(Basket)]);
+        } 
+          return View(basket);
+    }
+        
+
+}
