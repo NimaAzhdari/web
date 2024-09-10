c61ca68617485af19f39013fd19545599442d64e
NimaAzhdari <ajdarinima@gmail.com>
2024-09-10 00:47:13 -0700

    Add files via upload

---
+++ RazorPage/Data/ShopContext.cs
@@ -0,0 +1,14 @@
+﻿namespace MvcRazor.Data;
+using MvcRazor.Models;
+using Microsoft.EntityFrameworkCore;
+public class ShopContext :DbContext
+{
+   public DbSet<Product> Products { get; set; }
+   public DbSet<category_product> CategoryProduct { get; set; }
+   public DbSet<montakhab_product> MontakhabProduct { get; set; }
+   public DbSet<vitrin_product> VitrinProduct { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data source=neshat.db");
+        }
+}
